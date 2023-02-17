bac3182e8e76494b5670a53e57daf0174acbfaec
alekcangp <alekcangp@list.ru>
2023-02-17 07:15:44 +0000

    Update 15 files

--- components/LotteryCard.js
+++ components/LotteryCard.js
@@ -1,41 +1,60 @@
-import style from '../styles/PotCard.module.css'
-import truncateEthAddress from 'truncate-eth-address'
-import { useAppContext } from '../context/context'
+import style from "../styles/PotCard.module.css";
+import truncateEthAddress from "truncate-eth-address";
+import { useAppContext } from "../context/context";
 
 const LotteryCard = () => {
   // TODO: Get the data needed from context
-  const {enterLottery,lotteryPot,lotteryId,pickWinner,lastWinner,address,owner} =useAppContext()
+  const {
+    enterLottery,
+    lotteryPot,
+    lotteryId,
+    pickWinner,
+    lastWinner,
+    address,
+    owner,
+  } = useAppContext();
   return (
     <div className={style.wrapper}>
       <div className={style.title}>
         {/* TODO: Dynamically render the lotteryID */}
-        Lottery <span className={style.textAccent}>#{lotteryId ? lotteryId : '1'}</span>
+        Lottery{" "}
+        <span className={style.textAccent}>#{lotteryId ? lotteryId : "1"}</span>
       </div>
       <div className={style.pot}>
         {/* TODO: Dynamically render the lottery pot */}
-        Pot 🍯 : <span className={style.goldAccent}>{lotteryPot}</span>
+        Prize 💰: <span className={style.goldAccent}>{lotteryPot}</span>
       </div>
 
       <div className={style.recentWinnerTitle}>🏆 Last Winner 🏆</div>
       <div className={style.winner}>
         {/* TODO: Dynamically render the last winner */}
         {!lastWinner.length ? (
-        <div className={style.winner}>No winner yet</div>
-      ) : (
-        lastWinner.length > 0 && (
-          <div className={style.winner}>
-            {truncateEthAddress(lastWinner[lastWinner.length - 1])}
-          </div>
-        )
-      )}
+          <div className={style.winner}>No winner yet</div>
+        ) : (
+          lastWinner.length > 0 && (
+            <div className={style.winner}>
+              {truncateEthAddress(lastWinner[lastWinner.length - 1])}
+            </div>
+          )
+        )}
       </div>
       {/* TODO: Add onClick functionality to the buttons */}
       {address !== owner ? (
-        <div className={style.btn} onClick={enterLottery}>Enter </div>):(<div></div>)}
+        <div className={style.btn} onClick={enterLottery}>
+          Enter{" "}
+        </div>
+      ) : (
+        <div></div>
+      )}
 
-      {address === owner ?(
-        <div className={style.btn} onClick={pickWinner}>Pick Winner</div>):(<div></div>)}
+      {address === owner ? (
+        <div className={style.btn} onClick={pickWinner}>
+          Pick Winner
+        </div>
+      ) : (
+        <div></div>
+      )}
     </div>
-  )
-}
-export default LotteryCard
+  );
+};
+export default LotteryCard;
